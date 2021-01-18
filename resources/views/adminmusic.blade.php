b5c2a666efb98d95a3694c43aa4b16371d53b52e
DJLauwy <laudehoop@hotmail.com>
2021-01-18 19:43:32 +0100

    Mini update
    Mini update admin CMS

--- resources/views/adminmusic.blade.php
+++ resources/views/adminmusic.blade.php
@@ -5,5 +5,19 @@
 @endsection
 
 @section("content")
-<a href="{{ route('admin') }}"><p> < Terug</p></a>
+	<a href="{{ route('admin') }}"><p> < Terug</p></a>
+	@foreach($discographies as $track)
+		<div class="content__track">
+			<div>
+				<img src="{{ $track->cover }}" class="content__track-img">
+			</div>
+			<div>
+				<h3><b>Artist:</b> {{ $track->artist }} </h3>
+				<h4><b>Title:</b> {{ $track->title }} </h4>
+				<p><b>Genre:</b> {{ $track->genre }} <br>
+				<b>Released:</b> {{ $track->year }} </p>
+			</div>
+		</div>
+		<hr>
+	@endforeach
 @endsection
--- resources/views/adminnews.blade.php
+++ resources/views/adminnews.blade.php
@@ -5,5 +5,19 @@
 @endsection
 
 @section("content")
-<a href="{{ route('admin') }}"><p> < Terug</p></a>
+	<a href="{{ route('admin') }}"><p> < Terug</p></a>
+	<hr>
+	@foreach($news as $news)
+		<div class="content__track">
+			<div>
+				<img src="{{ $news->image }}" class="content__track-img">
+			</div>
+			<div>
+				<h3>{{ $news->title }}</h3>
+				<p><b>{{ $news->header }}</b></p>
+				<p>{{ $news->post }}</p>
+			</div>
+		</div>
+		<hr>
+	@endforeach
 @endsection
