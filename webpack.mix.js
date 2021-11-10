16fd4c6b99201b426030045bf376536bdf10401e
DJLauwy <laudehoop@hotmail.com>
2021-11-10 13:15:40 +0100

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,3 +19,5 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require("tailwindcss"),
     ]);
+
+mix.sass('resources/sass/app.scss', 'public/css');
